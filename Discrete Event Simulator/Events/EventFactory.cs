62b5f07f869e1addefdccbfdf883370e7913aa71
David Beath <davidgbeath@gmail.com>
2013-06-07 17:36:51 +1200

    Added EventFactory code

--- Discrete Event Simulator/Events/EventFactory.cs
+++ Discrete Event Simulator/Events/EventFactory.cs
@@ -1,6 +1,30 @@
-﻿namespace Discrete_Event_Simulator.Events
+﻿using Discrete_Event_Simulator.Entities;
+
+namespace Discrete_Event_Simulator.Events
 {
     public class EventFactory
     {
+        private Simulation Simulation;
+        private Random rGen;
+
+        public EventFactory(Simulation startSim, Random startRGen)
+        {
+            Simulation = startSim;
+            rGen = startRGen;
+        }
+
+        public Event CreateEvent(Constants.EventType eventType, Entity e)
+        {
+                switch (eventType)
+                {
+                    case Constants.EventType.Arrive:
+                        break;
+                    case Constants.EventType.JoinQueue:
+                        break;
+                    case Constants.EventType.CompleteService:
+                        break;
+
+                }
+        }
     }
 }
---
+++ Discrete Event Simulator/Random.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Discrete_Event_Simulator
+{
+    public class Random
+    {
+        public int CreateRandom(int range)
+        {
+            int randomValue;
+            return randomValue;
+        }
+    }
+}
